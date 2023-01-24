4804e87ffa1efb7029e47397e5f8a04834fef103
Кузнецов Антон <anton1202@yandex.ru>
2023-01-25 01:01:15 +0300

    1

--- tasks/prime.py
+++ tasks/prime.py
@@ -7,4 +7,11 @@ def is_prime(number: int) -> bool:
     """
     Функция должна вернуть True если число является простым, иначе - False
     """
-    raise NotImplementedError
+    if number == 0 or number == 1:
+        return False
+    for i in (2, number**(1/2)):
+        if number % i == 0:
+            return False
+        else:
+            return True
+
--- tasks/timestr.py
+++ tasks/timestr.py
@@ -12,7 +12,26 @@ def seconds_to_str(seconds: int) -> str:
     3700 -> 01h01m40s
     93600 -> 01d02h00m00s
     """
-    raise NotImplementedError
+    M = 60
+    H = 3600
+    D = 86400
+    if seconds >= D:
+        d = seconds // D
+        h = (seconds - (d * D)) // H
+        m = (seconds - (d * D) - (H * h)) // M
+        s = (seconds - (d * D) - (H * h) - (M * m))
+        return f'{d:02d}d{h:02d}h{m:02d}m{s:02d}s'
+    elif seconds >= H:
+        h = (seconds) // H
+        m = (seconds - (H * h)) // M
+        s = (seconds - (H * h) - (M * m))
+        return f'{h:02d}h{m:02d}m{s:02d}s'
+    elif seconds >= M:
+        m = seconds // M
+        s = seconds - (M * m)
+        return f'{m:02d}m{s:02d}s'
+    else:
+        return f'{seconds:02d}s'
 
 
 
